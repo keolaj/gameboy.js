bfedf54624c802d36d0657811ffa162c17986734
Solomon Jarvegren <keolajarvegren@gmail.com>
2019-12-05 23:22:00 -0800

    reset to black 2

--- gpu.js
+++ gpu.js
@@ -12,11 +12,10 @@ gpu = {
             if (gpu._canvasContext.createImageData) {
                 imageData = gpu._canvasContext.createImageData(160, 144);
                 for (let i = 0; i < imageData.data.length; i += 4) {
-                    // Modify pixel data
-                    imageData.data[i + 0] = 0;  // R value
-                    imageData.data[i + 1] = 0;    // G value
-                    imageData.data[i + 2] = 0;  // B value
-                    imageData.data[i + 3] = 255;  // A value
+                    imageData.data[i + 0] = 0;
+                    imageData.data[i + 1] = 0;
+                    imageData.data[i + 2] = 0;
+                    imageData.data[i + 3] = 255;
                 }
                 gpu._screen = imageData;
             } else if (gpu._canvasContext.getImageData) {
